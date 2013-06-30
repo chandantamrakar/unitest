6346d55bc0ae557f3003bbb585b669e286a2e353
chandan <ctamrakar@gmail.com>
2013-06-30 15:43:36 +0545

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unitest::Application.config.secret_token = '36432d2a742ea74eb63d61592c49a53215200339c2b8739acb64714e38d12d29c7e947c92b6cd7c7903a6f8c7d11dbb9a317934579ff419bb98266d32b75f67f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Unitest::Application.config.session_store :cookie_store, key: '_unitest_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Unitest::Application.config.session_store :active_record_store
